6d8a00beadaff8b253b71ae20277f4b0738ef17b
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-05-27 10:37:36 +0100

    base html extending upload, view pages

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/upload/')
-def index():
-    return 'Hello World! Index'
+def upload():
+    return render_template(url_for('upload.html'))
+
 
 @app.route('/select_folder/')
 def select_folder():
     return 'Hello World! Select Folder'
 
+
 @app.route('/query_db/')
 def query_db():
     return 'Hello World! Query DB'
+
 
 @app.route('/view_csv/')
 def view_csv():
     return 'Hello World! View CSV'
 
+
 @app.route('/select_view/')
 def select_view():
     return 'Hello World! Select View'
 
+
 @app.route('/results/')
 def results():
     return 'Hello World! Results'
+
 
 @app.route('/send_mail/', methods=['GET', 'POST'])
 def send_mail():
