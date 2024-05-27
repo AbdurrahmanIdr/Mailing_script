627bfd74c836da1d4000678e84eb93da69f3b298
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-05-27 12:18:13 +0100

    linking to folder path from upload dbquery html

--- app.py
+++ app.py
@@ -21,18 +21,20 @@ def detail_extract(file_page):
     return new_name
 
 
-def splitter(filename):
+def splitter(filename, folder_path):
     if filename.endswith('.pdf'):
         pdf_reader = PdfReader(filename)
         pages = len(pdf_reader.pages)
 
         for i in range(pages):
             pdf_writer = PdfWriter()
-            pdf_writer.addpage(pdf_reader.pages[i])
+            pdf_writer.add_page(pdf_reader.pages[i])
             name = detail_extract(pdf_reader.pages[i])
             pdf_writer.encrypt('12345')
             pdf_writer.write(name)
 
+        return True
+    return False
 
 
 @app.route("/")
@@ -45,14 +47,24 @@ def index():
 def upload():
     base_dir = os.path.abspath('data/raw')
     file = request.files['file']
+    filename = str(file.filename).split('.')[-1]
+    path = os.path.join(base_dir, filename)
+
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
     if file and file.filename != '':
         file.save(os.path.join(base_dir, file.filename))
         print(f'{file.filename} saved successfully at {base_dir}')
 
+        split = splitter(file, path)
+        if split:
+            print(f'{file.filename} saved successfully splitted and ecrypted at {path}')
+
     else:
         print(f'{file.filename} not saved at {base_dir}')
 
-    return render_template(url_for('query_db.html'))
+    return render_template(url_for('query_db.html', folder=path))
 
 
 @app.route('/select_folder/')
@@ -61,8 +73,8 @@ def select_folder():
 
 
 @app.route('/query_db/')
-def query_db():
-    return 'Hello World! Query DB'
+def query_db(folder):
+    return f'Hello World! {folder}'
 
 
 @app.route('/view_csv/')
