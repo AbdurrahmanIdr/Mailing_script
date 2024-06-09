52c29e521cbbfdca78c6c67fad96fde750dfea4d
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-06-09 18:11:28 +0100

    refactor: dispalay with corrected translation-x

--- static/scripts/base_script.js
+++ static/scripts/base_script.js
@@ -1,12 +1,9 @@
-// Fade out flash messages after 5 seconds
-        document.addEventListener('DOMContentLoaded', function () {
-            var flashes = document.querySelectorAll('.flash');
-            flashes.forEach(function (flash) {
-                setTimeout(function () {
-                    flash.style.opacity = '0';
-                    setTimeout(function () {
-                        flash.style.display = 'none';
-                    }, 1000);
-                }, 5000);
-            });
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    var flashes = document.querySelectorAll('.flash');
+    flashes.forEach(function (flash) {
+        setTimeout(function () {
+            flash.style.opacity = '0';
+            flash.style.display = 'none';
+        }, 5000);
+    });
+});
