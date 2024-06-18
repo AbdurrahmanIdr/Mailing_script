f50571427a3a263bd9a5762b5b47a462ef16a434
Abdurrahman Idris <abdurrahmaneedrees@gmail.com>
2024-06-18 20:38:27 +0100

    refactor: formated and escaped the error creation process for the empty, invalid and error files

--- models/pdf_rel.py
+++ models/pdf_rel.py
@@ -12,43 +12,47 @@ progress = dict()
 
 
 def detail_extract(file_page):
-    contents = file_page.extract_text().split('\n')
-    content = contents[3:6]
+    try:
+        contents = file_page.extract_text().split('\n')
+        content = contents[3:6]
 
-    dates = content[0].strip().split('-')
-    d_mon = dates[0]
-    d_year = dates[-1]
-    sur_name = content[1].split(':')[1].split(',')[0].strip()
-    if ':' in content[-1]:
-        ippis = content[-1].split(':')[-1].strip()
-    else:
-        ippis = content[6].split(':')[-1].strip()
-    new_name = [ippis, sur_name, d_mon, d_year]
-    return new_name
+        dates = content[0].strip().split('-')
+        d_mon = dates[0]
+        d_year = dates[-1]
+        sur_name = content[1].split(':')[1].split(',')[0].strip()
+        if ':' in content[-1]:
+            ippis = content[-1].split(':')[-1].strip()
+        else:
+            ippis = content[6].split(':')[-1].strip()
+        new_name = [ippis, sur_name, d_mon, d_year]
+        return new_name
+    except Exception:
+        return None
 
 
-# Splitter function to process PDF
-def splitter(filename, task_id):
-    if str(filename).endswith('.pdf'):
-        file_folder = filename.split('.')[0]
-        folder_path = os.path.join(base_dir, file_folder)
+def splitter(file, file_path, task_id):
+    try:
+        if os.path.exists(file_path):
+            shutil.rmtree(file_path)
 
-        if os.path.exists(folder_path):
-            shutil.rmtree(folder_path)
-
-        os.makedirs(folder_path)
-
-        file_path = str(os.path.realpath(filename))
-        pdf_reader = PdfReader(file_path)
+        os.makedirs(file_path)
+        
+        pdf_reader = PdfReader(file)
         pages = len(pdf_reader.pages)
 
-        start = time.time()
-
         for i in range(pages):
+            name = detail_extract(pdf_reader.pages[i])
+            
+            # Update progress
+            progress[task_id] = (i + 1) / pages * 100
+            time.sleep(0.1)  # Simulate time taken for processing each page
+            
+            if not name:
+                continue
+            
             pdf_writer = PdfWriter()
             pdf_writer.add_page(pdf_reader.pages[i])
-            name = detail_extract(pdf_reader.pages[i])
-
+            
             if name[0] == '482427':
                 name[0] = str(int(name[0]) + i)
 
@@ -57,17 +61,13 @@ def splitter(filename, task_id):
 
             name = f'{name[0]}_{name[1]}_{name[2]}-{name[3]}.pdf'
 
-            file_name = str(os.path.join(base_dir, filename.split('.')[0], name))
+            file_name = str(os.path.join(file_path, name))
             with open(file_name, 'wb') as f:
                 pdf_writer.write(f)
 
-            # Update progress
-            progress[task_id] = (i + 1) / pages * 100
-            time.sleep(0.1)  # Simulate time taken for processing each page
-
         progress[task_id] = 100  # Ensure progress is marked complete
-        stop = time.time()
-        total_time = stop - start
-        print(f'Total time: {total_time}')
         return True
-    return False
+    except Exception as e:
+        progress[task_id] = 100  # Indicate error
+        print('error:', e)
+        return False
